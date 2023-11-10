36c80a378de1d40e467ad3a6f78c7f4b62afe657
jeffubayi <jeffubayi@gmail.com>
2023-11-10 18:07:54 +0300

    fix cors issue

--- backend/server.js
+++ backend/server.js
@@ -7,31 +7,36 @@ const cors = require("cors");
 const userRoute = require("./routes/userRoute");
 const productRoute = require("./routes/productRoute");
 const contactRoute = require("./routes/contactRoute");
-const errorHandler = require('./middleware/errorMiddleware');
-const cookieParser = require('cookie-parser');
+const errorHandler = require("./middleware/errorMiddleware");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 
 //Middlewares
 app.use(express.json());
-app.use(cookieParser())
-app.use(express.urlencoded({extended: false}))
-app.use(bodyParser.json())
-app.use(cors({
-  origin: ['http://localhost:3000', 'https://invent-app.vercel.app'],
-  credentials: true
-}));
-app.use("/uploads", express.static(path.join(__dirname, "uploads")))
-
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "https://sortly-inventory.vercel.app",
+      "https://sortly.onrender.com",
+    ],
+    credentials: true,
+  })
+);
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 // Routes Middleware
-app.use("/api/users", userRoute)
-app.use("/api/products", productRoute)
-app.use("/api/contactus", contactRoute)
+app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
+app.use("/api/contactus", contactRoute);
 
 //Routes
-app.get('/', (req,res) => {
-  res.send('Logicea API')
+app.get("/", (req, res) => {
+  res.send("SORTLY INVENTORY API");
 });
 
 //Error Middleware
@@ -39,8 +44,11 @@ app.use(errorHandler);
 
 // Connect to DB and start server
 const PORT = process.env.PORT || 5000;
-mongoose.connect(process.env.MONGO_URI).then(() => {
-  app.listen(PORT, () => {
-    console.log(`Server running at port: ${PORT}`)
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running at port: ${PORT}`);
+    });
   })
-}).catch((err) => console.log(err))
+  .catch((err) => console.log(err));
