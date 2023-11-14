b4895b4d58f5775f628386cf9660754b47ed7093
jeffubayi <jeffubayi@gmail.com>
2023-11-14 19:33:05 +0300

    feat:add reusable components

--- frontend/components/Buttons/index.tsx
+++
@@ -1,2 +0,0 @@
-export { default as LogoButton } from './iconButton';
-export { default as MainButton } from './mainButton';
--- frontend/components/TextFields/index.tsx
+++
@@ -1,2 +0,0 @@
-export { default as InputField } from './inputField';
-export { default as SelectField } from './selectField';
