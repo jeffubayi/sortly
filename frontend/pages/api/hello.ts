095363e0e217a18355faf9f719a8a56185cfa355
jeffubayi <jeffubayi@gmail.com>
2023-11-14 19:35:16 +0300

    add routes

--- frontend/pages/api/hello.ts
+++
@@ -1,8 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-import { getUserFromContext } from '../../utility/serverUser';
-
-export  const createContext = async(req: NextApiRequest, res: NextApiResponse) => {
-  // const user = await getUserFromContext(ctx);
-
-  // do anything you want with it
-};
