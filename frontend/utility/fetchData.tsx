e7c13f7cc3316d820bb8728ab3deaf922a6f2ea6
jeffubayi <jeffubayi@gmail.com>
2023-11-14 19:34:29 +0300

    refactor code

--- frontend/utility/fetchData.tsx
+++
@@ -1,7 +0,0 @@
-import { supabase } from "./supabaseClient";
-
-const fetchData = async (collection: string) => {
-    const { data } = await supabase.from(`${collection}`).select(`*`)
-    return data
-}
-export default fetchData
